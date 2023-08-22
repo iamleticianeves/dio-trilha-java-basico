6842292b6810e57de964393246cff7241f777af3
Letícia Neves <iamleticianeves@gmail.com>
2023-08-22 16:20:21 -0300

    first commit!

---
+++ src/Contador.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+// Classe da exceção personalizada
+class ParametrosInvalidosException extends Exception {
+    public ParametrosInvalidosException(String message) {
+        super(message);
+    }
+}
+
+public class Contador {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite o primeiro parâmetro: ");
+        int parametroUm = scanner.nextInt();
+        System.out.println("Digite o segundo parâmetro: ");
+        int parametroDois = scanner.nextInt();
+
+        scanner.close();
+
+        try {
+
+            contar(parametroUm, parametroDois);
+
+        } catch (ParametrosInvalidosException exception) {
+
+            System.out.println(exception.getMessage());
+        }
+    }
+
+    static void contar(int parametroUm, int parametroDois) throws ParametrosInvalidosException {
+
+        if (parametroUm > parametroDois) {
+            throw new ParametrosInvalidosException("O segundo parâmetro deve ser maior que o primeiro!");
+        }
+
+        int contagem = parametroDois - parametroUm;
+
+        for (int i = 1; i <= contagem; i++) {
+            System.out.println("Imprimindo o número " + i);
+        }
+    }
+}
